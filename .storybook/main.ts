e54718347487a584dd4d6b6b58a54788fbcb03c5
Dao Phan <daopk.dev@gmail.com>
2026-07-20 17:24:29 +0700

    chore(storybook): add pseudo-states addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,12 @@ import type { StorybookConfig } from '@storybook/vue3-vite';
 
 const config: StorybookConfig = {
     stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-    addons: ['@storybook/addon-a11y', '@storybook/addon-themes', '@storybook/addon-vitest'],
+    addons: [
+        '@storybook/addon-a11y',
+        '@storybook/addon-themes',
+        '@storybook/addon-vitest',
+        'storybook-addon-pseudo-states',
+    ],
     framework: '@storybook/vue3-vite',
     viteFinal: async (viteConfig) => ({
         ...viteConfig,
