9ff6e10503a26b91177880832a0f28ae1e217fa6
Dao Phan <daopk.dev@gmail.com>
2026-08-08 03:40:26 +0700

    feat(vue): add the chevron right icon

---
+++ packages/vue/src/components/icons/icon-chevron-right.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts" vapor>
+defineProps<{class?: string}>();
+</script>
+
+<template>
+  <svg
+    aria-hidden="true"
+    :class="$props.class"
+    fill="none"
+    height="16"
+    role="presentation"
+    viewBox="0 0 16 16"
+    width="16"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      clip-rule="evenodd"
+      d="M5.47 2.97a.75.75 0 0 1 1.06 0l4.5 4.5a.75.75 0 0 1 0 1.06l-4.5 4.5a.75.75 0 1 1-1.06-1.06L9.44 8 5.47 4.03a.75.75 0 0 1 0-1.06Z"
+      fill="currentColor"
+      fill-rule="evenodd"
+    />
+  </svg>
+</template>
--- packages/vue/src/components/icons/index.ts
+++ packages/vue/src/components/icons/index.ts
@@ -1,2 +1,3 @@
 export {default as IconChevronDown} from "./icon-chevron-down.vue";
+export {default as IconChevronRight} from "./icon-chevron-right.vue";
 export {default as IconClose} from "./icon-close.vue";
