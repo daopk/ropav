f46507acfaf6cdc2fe14683be635d716f66ad14d
Dao Phan <daopk.dev@gmail.com>
2026-08-08 15:20:47 +0700

    feat(vue): add the search icon

--- packages/vue/src/components/icons/index.ts
+++ packages/vue/src/components/icons/index.ts
@@ -1,3 +1,4 @@
 export {default as IconChevronDown} from "./icon-chevron-down.vue";
 export {default as IconChevronRight} from "./icon-chevron-right.vue";
 export {default as IconClose} from "./icon-close.vue";
+export {default as IconSearch} from "./icon-search.vue";
