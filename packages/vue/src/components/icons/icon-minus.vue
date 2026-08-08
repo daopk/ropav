56a5955639ef0e0ddd28b06511d3f4909caf8396
Dao Phan <daopk.dev@gmail.com>
2026-08-08 16:47:48 +0700

    feat(vue): add the plus and minus icons

---
+++ packages/vue/src/components/icons/icon-minus.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts" vapor>
+defineProps<{class?: string}>();
+</script>
+
+<template>
+  <svg
+    aria-hidden="true"
+    :class="$props.class"
+    fill="none"
+    height="16"
+    role="presentation"
+    viewBox="0 0 16 16"
+    width="16"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      clip-rule="evenodd"
+      d="M1.75 8a.75.75 0 0 1 .75-.75h11a.75.75 0 0 1 0 1.5h-11A.75.75 0 0 1 1.75 8"
+      fill="currentColor"
+      fill-rule="evenodd"
+    />
+  </svg>
+</template>
--- packages/vue/src/components/icons/index.ts
+++ packages/vue/src/components/icons/index.ts
@@ -2,4 +2,6 @@ export {default as ExternalLinkIcon} from "./external-link-icon.vue";
 export {default as IconChevronDown} from "./icon-chevron-down.vue";
 export {default as IconChevronRight} from "./icon-chevron-right.vue";
 export {default as IconClose} from "./icon-close.vue";
+export {default as IconMinus} from "./icon-minus.vue";
+export {default as IconPlus} from "./icon-plus.vue";
 export {default as IconSearch} from "./icon-search.vue";
