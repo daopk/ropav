8216f1872c93a69ea76167bfb918cfdfc4d32db7
Dao Phan <daopk.dev@gmail.com>
2026-09-07 13:13:33 +0700

    refactor(styles)!: three utility strings for a toolchain the package no longer runs go, the classes they name having been CSS since 0.10.0

--- packages/styles/src/index.ts
+++ packages/styles/src/index.ts
@@ -1,9 +1,6 @@
 // The type recipes derive their prop unions from; `tv` itself stays internal.
 export type { VariantProps } from "./tv";
 
-// Export utility classes
-export * from "./utils";
-
 // Export all component variants
 export * from "./components";
 
